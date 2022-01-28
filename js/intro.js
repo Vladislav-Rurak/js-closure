2b5c975dd50f194e2c8fddbbfa335062453fdfec
bigbadabum6 <dolgkozly@gmail.com>
2022-01-28 15:44:02 +0200

    last commit

--- js/intro.js
+++ js/intro.js
@@ -24,26 +24,3 @@ console.log(
   "counter1() :>> ",
   counter1()
 );
-
-const counter2 = counter();
-console.log(
-  "counter2() :>> ",
-  counter2()
-);
-console.log(
-  "counter2() :>> ",
-  counter2()
-);
-console.log(
-  "counter2() :>> ",
-  counter2()
-);
-console.log(
-  "counter2() :>> ",
-  counter2()
-);
-
-console.log(
-  "counter1() :>> ",
-  counter1()
-);
